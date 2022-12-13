73e8d5ae0247fd788a7a03037df356ab242d52a2
damilolajohn6 <damilolajohn622@gmail.com>
2022-12-12 21:56:59 -0800

    first putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/** 
+  * main - Entry point 
+  * 
+  * Description: prints _putchar using putchar prototype 
+  * 
+  * Return: Always 0 (Success) 
+ */ 
+  
+int main(void) 
+{ 
+char dam[] = "_putchar"; 
+int ch;
+  
+for (xy = 0; xy < 8; ++xy) 
+_putchar(dam[xy]); 
+_putchar('\n'); 
+  
+return (0); 
+}
