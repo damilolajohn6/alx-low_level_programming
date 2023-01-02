d17611dff9562af3143c6d232519c11fafacf8e4
damilolajohn6 <damilolajohn622@gmail.com>
2023-01-02 11:44:56 -0800

    strings break in pointers

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,27 +1,26 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _strpbrk - locates the first occurrence in the string s of any of
- * the bytes in the string accept.
- * @s: string to pick char
- * @accept: string to compare to
- * Return: string pointer
+ * _strpbrk -  a function that searches a string for any of a set of bytes.
+ * @s: an input string
+ * @accept: an input character with to locate into string s
+ * Return:  a pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-	char *start = accept;
+	int index;
 
 	while (*s)
 	{
-		while (*accept)
+		for (index = 0; accept[index]; index++)
 		{
-			if (*accept == *s)
+			if (*s == accept[index])
 				return (s);
-			accept++;
 		}
-
-		accept = start;
 		s++;
 	}
-	return (NULL);
+	return ('\0');
 }
