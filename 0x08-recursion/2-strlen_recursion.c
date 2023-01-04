2c6691e9675eb987bbeda0ac20ba7a067d71f5cd
damilolajohn6 <damilolajohn622@gmail.com>
2023-01-04 00:08:07 -0800

    string lenght

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+
+#include "main.h"
+/**
+ * _strlen_recursion - strlen with recursion
+ * @s: string to claculate length
+ * Return: void
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
