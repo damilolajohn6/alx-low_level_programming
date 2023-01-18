c26fd1c00d9d8e1cd9649c19fb7fe8379a886927
damilolajohn6 <damilolajohn622@gmail.com>
2023-01-18 03:11:23 -0800

    i dont have to think too much over a thing

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+/**
+ *  * array_iterator - execute function on each element
+ *   * of an array
+ *    * @array: given array
+ *     * @size: size of the array
+ *      * @action: pointer to function
+ *       * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
