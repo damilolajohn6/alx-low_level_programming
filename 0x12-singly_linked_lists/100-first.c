bcbb68d642baff4c162c1e3a97bcaebf2ef545a2
damilolajohn6 <damilolajohn622@gmail.com>
2023-01-26 10:54:58 -0800

    advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-void __attribute__((constructor)) index(void);
-
+void __attribute__((constructor)) premain(void);
 /**
- * hare - Prints a string before the
- *   main function is executed.
+ * premain - Prints a string before the
  */
-void index(void)
+void premain(void)
 {
-printf("You're beat! and yet, you must allow,\n"
-"I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
